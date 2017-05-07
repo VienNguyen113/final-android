67ea05fb3f04316a9fb82558783d991b167046c2
hoangddt <quochoangddt@gmail.com>
2017-05-07 12:09:17 +0700

    Add home screen and button handler

---
+++ src/DuoiHinhBatChu/app/src/main/java/com/hoangddt/duoihinhbatchu/AppActivity.java
@@ -0,0 +1,9 @@
+package com.hoangddt.duoihinhbatchu;
+
+import android.app.Activity;
+
+/**
+ * Created by hoangddt on 07/05/2017.
+ */
+public class AppActivity extends Activity {
+}
--- src/DuoiHinhBatChu/app/src/main/java/com/hoangddt/duoihinhbatchu/MainActivity.java
+++ src/DuoiHinhBatChu/app/src/main/java/com/hoangddt/duoihinhbatchu/MainActivity.java
@@ -2,12 +2,68 @@ package com.hoangddt.duoihinhbatchu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    Button mGuide, mSetting, mInfo, mStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate() called");
+        //Remove title bar
+        // this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        //Remove notification bar
+        // this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // remove name
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_app);
+
+
+        // Init and setting button
+        mGuide = (Button) findViewById(R.id.huong_dan);
+        mGuide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // do stuff here
+                Log.d(TAG, "Huong Dan called");
+            }
+        });
+
+        mInfo = (Button) findViewById(R.id.thong_tin);
+        mInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // stuff here
+                Log.d(TAG, "Info called");
+            }
+        });
+
+        mSetting = (Button) findViewById(R.id.cai_dat);
+        mSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // stuff here
+                Log.d(TAG, "Setting called");
+            }
+        });
+
+        mStart = (Button) findViewById(R.id.bat_dau);
+        mStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // stuff here
+                Log.d(TAG, "Start called");
+            }
+        });
     }
 }
